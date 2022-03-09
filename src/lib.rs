633e40d8c80c281a681ec691412d03e4fe22f76a
SamuelLFA <samuellfa3@gmail.com>
2022-03-09 08:48:25 -0300

    expect panic

--- src/lib.rs
+++ src/lib.rs
@@ -57,8 +57,46 @@
 //     }
 // }
 
-pub fn greeting(name: &str) -> String {
-    String::from("Hello!")
+// pub fn greeting(name: &str) -> String {
+//     String::from("Hello!")
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     fn greeting_contains_name() {
+//         let result = greeting("Carol");
+//         assert!(
+//             result.contains("Carol"),
+//             "Greeting did not contain name, values was `{}`",
+//             result
+//         );
+//     }
+// }
+
+pub struct Guess {
+    value: i32,
+}
+
+// --snip--
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }
+
+        Guess { value }
+    }
 }
 
 #[cfg(test)]
@@ -66,12 +104,8 @@ mod tests {
     use super::*;
 
     #[test]
-    fn greeting_contains_name() {
-        let result = greeting("Carol");
-        assert!(
-            result.contains("Carol"),
-            "Greeting did not contain name, values was `{}`",
-            result
-        );
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
