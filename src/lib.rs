11026be37f431be37ed767c688e88d2d38979dd7
SamuelLFA <samuellfa3@gmail.com>
2022-03-09 08:50:14 -0300

    test using result

--- src/lib.rs
+++ src/lib.rs
@@ -76,36 +76,48 @@
 //     }
 // }
 
-pub struct Guess {
-    value: i32,
-}
+// pub struct Guess {
+//     value: i32,
+// }
 
-// --snip--
-impl Guess {
-    pub fn new(value: i32) -> Guess {
-        if value < 1 {
-            panic!(
-                "Guess value must be greater than or equal to 1, got {}.",
-                value
-            );
-        } else if value > 100 {
-            panic!(
-                "Guess value must be less than or equal to 100, got {}.",
-                value
-            );
-        }
+// // --snip--
+// impl Guess {
+//     pub fn new(value: i32) -> Guess {
+//         if value < 1 {
+//             panic!(
+//                 "Guess value must be greater than or equal to 1, got {}.",
+//                 value
+//             );
+//         } else if value > 100 {
+//             panic!(
+//                 "Guess value must be less than or equal to 100, got {}.",
+//                 value
+//             );
+//         }
 
-        Guess { value }
-    }
-}
+//         Guess { value }
+//     }
+// }
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+
+//     #[test]
+//     #[should_panic(expected = "Guess value must be less than or equal to 100")]
+//     fn greater_than_100() {
+//         Guess::new(200);
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
-    #[should_panic(expected = "Guess value must be less than or equal to 100")]
-    fn greater_than_100() {
-        Guess::new(200);
+    fn it_works() -> Result<(), String> {
+        if 2 + 2 == 5 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 }
